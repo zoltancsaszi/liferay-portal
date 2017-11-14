a538dda8da08ba340d8f1eec5e8becbd7e644499
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-14 15:01:16 +0100

    LPS-75893 getDataLevel - WikiDisplayPortletDataHandler

--- modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/internal/exportimport/data/handler/WikiDisplayPortletDataHandler.java
+++ modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/internal/exportimport/data/handler/WikiDisplayPortletDataHandler.java
@@ -43,13 +43,17 @@ public class WikiDisplayPortletDataHandler extends BasePortletDataHandler {
 	public static final String SCHEMA_VERSION = "1.0.0";
 
 	@Override
+	public DataLevel getDataLevel() {
+		return DataLevel.PORTLET_INSTANCE;
+	}
+
+	@Override
 	public String getSchemaVersion() {
 		return SCHEMA_VERSION;
 	}
 
 	@Activate
 	protected void activate() {
-		setDataLevel(DataLevel.PORTLET_INSTANCE);
 		setDataPortletPreferences("title", "nodeId");
 		setExportControls(new PortletDataHandlerControl[0]);
 	}
