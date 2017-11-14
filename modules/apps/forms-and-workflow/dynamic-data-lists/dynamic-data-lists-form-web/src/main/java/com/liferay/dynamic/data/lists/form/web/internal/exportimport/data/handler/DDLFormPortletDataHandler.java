c9a5536236ed2056629d60666fcc6662319d76fc
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-14 15:40:00 +0100

    LPS-75893 getDataLevel - DDLFormPortletDataHandler

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/exportimport/data/handler/DDLFormPortletDataHandler.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/exportimport/data/handler/DDLFormPortletDataHandler.java
@@ -44,13 +44,17 @@ public class DDLFormPortletDataHandler extends BasePortletDataHandler {
 	public static final String SCHEMA_VERSION = "1.0.0";
 
 	@Override
+	public DataLevel getDataLevel() {
+		return DataLevel.PORTLET_INSTANCE;
+	}
+	
+	@Override
 	public String getSchemaVersion() {
 		return SCHEMA_VERSION;
 	}
 
 	@Activate
 	protected void activate() {
-		setDataLevel(DataLevel.PORTLET_INSTANCE);
 		setDataPortletPreferences("recordSetId");
 		setExportControls(new PortletDataHandlerControl[0]);
 	}
