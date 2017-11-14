bf832ad1e873741b79de8df45538ae9507ce01cd
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-14 15:23:11 +0100

    LPS-75893 getDataLevel - LayoutPrototypePortletDataHandler

--- modules/apps/web-experience/layout/layout-prototype-impl/src/main/java/com/liferay/layout/prototype/internal/exportimport/data/handler/LayoutPrototypePortletDataHandler.java
+++ modules/apps/web-experience/layout/layout-prototype-impl/src/main/java/com/liferay/layout/prototype/internal/exportimport/data/handler/LayoutPrototypePortletDataHandler.java
@@ -53,13 +53,17 @@ public class LayoutPrototypePortletDataHandler extends BasePortletDataHandler {
 	public static final String SCHEMA_VERSION = "1.0.0";
 
 	@Override
+	public DataLevel getDataLevel() {
+		return DataLevel.PORTAL;
+	}
+
+	@Override
 	public String getSchemaVersion() {
 		return SCHEMA_VERSION;
 	}
 
 	@Activate
 	protected void activate() {
-		setDataLevel(DataLevel.PORTAL);
 		setDeletionSystemEventStagedModelTypes(
 			new StagedModelType(LayoutPrototype.class));
 		setExportControls(
