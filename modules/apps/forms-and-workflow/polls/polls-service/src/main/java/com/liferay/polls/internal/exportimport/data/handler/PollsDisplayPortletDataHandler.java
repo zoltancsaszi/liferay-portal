a783574f276e46c7a0a07ee77b9a53b1f34ecd34
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-14 15:21:25 +0100

    LPS-75893 getDataLevel - PollsDisplayPortletDataHandler

--- modules/apps/forms-and-workflow/polls/polls-service/src/main/java/com/liferay/polls/internal/exportimport/data/handler/PollsDisplayPortletDataHandler.java
+++ modules/apps/forms-and-workflow/polls/polls-service/src/main/java/com/liferay/polls/internal/exportimport/data/handler/PollsDisplayPortletDataHandler.java
@@ -43,13 +43,17 @@ public class PollsDisplayPortletDataHandler extends BasePortletDataHandler {
 	public static final String SCHEMA_VERSION = "1.0.0";
 
 	@Override
+	public DataLevel getDataLevel() {
+		return DataLevel.PORTLET_INSTANCE;
+	}
+
+	@Override
 	public String getSchemaVersion() {
 		return SCHEMA_VERSION;
 	}
 
 	@Activate
 	protected void activate() {
-		setDataLevel(DataLevel.PORTLET_INSTANCE);
 		setDataPortletPreferences("questionId");
 		setExportControls(new PortletDataHandlerControl[0]);
 		setPublishToLiveByDefault(
