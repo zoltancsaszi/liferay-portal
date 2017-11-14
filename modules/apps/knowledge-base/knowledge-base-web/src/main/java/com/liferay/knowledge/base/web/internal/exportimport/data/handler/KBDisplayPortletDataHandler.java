0f4e1b5043c95d1683fe409ec2d6d7e136b82653
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-14 15:57:34 +0100

    LPS-75893 getDataLevel - KBDisplayPortletDataHandler

--- modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/exportimport/data/handler/KBDisplayPortletDataHandler.java
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/exportimport/data/handler/KBDisplayPortletDataHandler.java
@@ -42,13 +42,17 @@ public class KBDisplayPortletDataHandler extends BasePortletDataHandler {
 	public static final String SCHEMA_VERSION = "2.0.0";
 
 	@Override
+	public DataLevel getDataLevel() {
+		return DataLevel.PORTLET_INSTANCE;
+	}
+
+	@Override
 	public String getSchemaVersion() {
 		return SCHEMA_VERSION;
 	}
 
 	@Activate
 	protected void activate() {
-		setDataLevel(DataLevel.PORTLET_INSTANCE);
 		setDataPortletPreferences("resourceClassNameId", "resourcePrimKey");
 		setExportControls(new PortletDataHandlerControl[0]);
 	}
