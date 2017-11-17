1f481f035c0d58bfb57e58c8526ef7158b3aa21f
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-17 13:31:23 +0100

    LPS-75933 PortletDataHandlerTests - AdminPortletDataHandlerTest

---
+++ modules/apps/wsrp/wsrp-test/src/testIntegration/java/com/liferay/wsrp/web/internal/exportimport/data/handler/test/AdminPortletDataHandlerTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wsrp.web.internal.exportimport.data.handler.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.exportimport.kernel.lar.DataLevel;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.wsrp.constants.WSRPPortletKeys;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Zsolt Berentey
+ */
+@RunWith(Arquillian.class)
+public class AdminPortletDataHandlerTest
+	extends BasePortletDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Override
+	protected void addStagedModels() {
+	}
+
+	@Override
+	protected DataLevel getDataLevel() {
+		return DataLevel.PORTAL;
+	}
+
+	@Override
+	protected String getPortletId() {
+		return WSRPPortletKeys.WSRP_ADMIN;
+	}
+
+	@Override
+	protected boolean isDataPortalLevel() {
+		return true;
+	}
+
+	@Override
+	protected boolean isDataPortletInstanceLevel() {
+		return false;
+	}
+
+	@Override
+	protected boolean isDataSiteLevel() {
+		return false;
+	}
+
+}
