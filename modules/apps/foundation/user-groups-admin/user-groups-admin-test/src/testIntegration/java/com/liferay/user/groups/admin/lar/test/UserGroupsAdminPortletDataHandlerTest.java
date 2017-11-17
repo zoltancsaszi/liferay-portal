0cdff715e47b010c1ea65b26a1b38e7825044d2d
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-17 13:07:18 +0100

    LPS-75933 PortletDataHandlerTests - UserGroupsAdminPortletDataHandlerTest

--- modules/apps/foundation/user-groups-admin/user-groups-admin-test/src/testIntegration/java/com/liferay/user/groups/admin/lar/test/UserGroupsAdminPortletDataHandlerTest.java
+++ modules/apps/foundation/user-groups-admin/user-groups-admin-test/src/testIntegration/java/com/liferay/user/groups/admin/lar/test/UserGroupsAdminPortletDataHandlerTest.java
@@ -15,6 +15,7 @@
 package com.liferay.user.groups.admin.lar.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.exportimport.kernel.lar.DataLevel;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -41,8 +42,28 @@ public class UserGroupsAdminPortletDataHandlerTest
 	}
 
 	@Override
+	protected DataLevel getDataLevel() {
+		return DataLevel.PORTAL;
+	}
+
+	@Override
 	protected String getPortletId() {
 		return UserGroupsAdminPortletKeys.USER_GROUPS_ADMIN;
+	}
+
+	@Override
+	protected boolean isDataPortalLevel() {
+		return true;
+	}
+
+	@Override
+	protected boolean isDataPortletInstanceLevel() {
+		return false;
+	}
+
+	@Override
+	protected boolean isDataSiteLevel() {
+		return false;
 	}
 
 }
