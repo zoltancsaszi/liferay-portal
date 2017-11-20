fbb3ec934d97699632dac638c2871a84ff74a1b7
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-20 15:49:31 +0100

    LPS-76011 WikiPortletDataHandlerTest

--- modules/apps/collaboration/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/web/internal/exportimport/data/handler/test/WikiPortletDataHandlerTest.java
+++ modules/apps/collaboration/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/web/internal/exportimport/data/handler/test/WikiPortletDataHandlerTest.java
@@ -16,6 +16,7 @@ package com.liferay.wiki.web.internal.exportimport.data.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.exportimport.kernel.lar.DataLevel;
+import com.liferay.exportimport.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
@@ -25,6 +26,7 @@ import com.liferay.portal.service.test.ServiceTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.wiki.constants.WikiPortletKeys;
 import com.liferay.wiki.model.WikiNode;
+import com.liferay.wiki.model.WikiPage;
 import com.liferay.wiki.util.test.WikiTestUtil;
 
 import org.junit.Before;
@@ -72,6 +74,14 @@ public class WikiPortletDataHandlerTest extends BasePortletDataHandlerTestCase {
 	@Override
 	protected String[] getDataPortletPreferences() {
 		return new String[] {"hiddenNodes, visibleNodes"};
+	}
+
+	@Override
+	protected StagedModelType[] getDeletionSystemEventStagedModelTypes() {
+		return new StagedModelType[] {
+			new StagedModelType(WikiNode.class),
+			new StagedModelType(WikiPage.class)
+		};
 	}
 
 	@Override
