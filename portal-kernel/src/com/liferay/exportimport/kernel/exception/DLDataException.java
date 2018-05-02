379bec69dee08490ef2d86de747b99ae83dec2ff
Zoltán Császi <zoltan.csaszi@liferay.com>
2018-05-02 13:17:21 +0200

    LPS-78419 portal-kernel DLDataException modified

--- portal-kernel/src/com/liferay/exportimport/kernel/exception/DLDataException.java
+++ portal-kernel/src/com/liferay/exportimport/kernel/exception/DLDataException.java
@@ -21,27 +21,49 @@ import com.liferay.exportimport.kernel.lar.PortletDataException;
  */
 public class DLDataException extends PortletDataException {
 
-	public static final int CMIS_FILE_NAME_IS_NULL = 990;
 	public static final int CMIS_FILE_DUPLICATED = 991;
+
+	public static final int CMIS_FILE_NAME_IS_NULL = 990;
+
 	public static final int CMIS_FOLDER_DUPLICATED = 992;
-	public static final int INVALID_FOLDER_NAME = 91;
-	public static final int INVALID_FILE_NAME = 92;
-	public static final int INVALID_FILE_SIZE = 93;
-	public static final int INVALID_SOURCE_FIlE_EXTENSION = 94;
-	public static final int INVALID_FILE_VERSION_LABEL = 95;
-	public static final int INVALID_ROOT_FOLDER = 96;
-	public static final int UNABLE_TO_UPDATE_PORTLET_PREFERENCES = 97;
-	public static final int FILE_TITLE_IS_NULL = 98;
-	public static final int PWC_FILE_VERSION = 99;
-	public static final int IMAGE_SIZE_EXCEPTION = 100;
-	public static final int NO_SUCH_FOLDER = 101;
-	public static final int DUPLICATED_FOLDER_NAME = 102;
-	public static final int DUPLICATED_FILE_NAME = 103;
-	public static final int INVALID_FILE_ENTRY_TYPE = 104;
-	public static final int FILE_EXTENSION_EXCEEDS_LIMIT = 105;
-	public static final int REQUIRED_FILE_ENTRY_TYPE = 106;
+
+	public static final int CMIS_NO_SUCH_FOLDER = 993;
+
 	public static final int DUPLICATED_FILE_ENTRY_TYPE = 107;
+
+	public static final int DUPLICATED_FILE_NAME = 103;
+
+	public static final int DUPLICATED_FOLDER_NAME = 102;
+
+	public static final int FILE_EXTENSION_EXCEEDS_LIMIT = 105;
+
+	public static final int FILE_TITLE_IS_NULL = 98;
+
+	public static final int IMAGE_SIZE_EXCEPTION = 100;
+
+	public static final int INVALID_FILE_ENTRY_TYPE = 104;
+
+	public static final int INVALID_FILE_NAME = 92;
+
+	public static final int INVALID_FILE_SIZE = 93;
+
+	public static final int INVALID_FILE_VERSION_LABEL = 95;
+
+	public static final int INVALID_FOLDER_NAME = 91;
+
+	public static final int INVALID_ROOT_FOLDER = 96;
+
+	public static final int INVALID_SOURCE_FILE_EXTENSION = 94;
+
+	public static final int NO_SUCH_FOLDER = 101;
+
 	public static final int NO_SUCH_METADATA_SET = 108;
+
+	public static final int PWC_FILE_VERSION = 99;
+
+	public static final int REQUIRED_FILE_ENTRY_TYPE = 106;
+
+	public static final int UNABLE_TO_UPDATE_PORTLET_PREFERENCES = 97;
 
 	public DLDataException(int type) {
 		setType(type);
@@ -49,6 +71,7 @@ public class DLDataException extends PortletDataException {
 
 	public DLDataException(int type, Throwable cause) {
 		super(cause);
+
 		setType(type);
 	}
 
@@ -61,4 +84,5 @@ public class DLDataException extends PortletDataException {
 	}
 
 	private String[] _data;
+
 }
