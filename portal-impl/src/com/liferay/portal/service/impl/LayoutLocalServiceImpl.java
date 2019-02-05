c107a12f8c75b956a36e6884f44011875ca0736e
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-02-05 15:01:34 +0100

    LPS-89170 Use head=true whenever a client calls for a layout set

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -1661,7 +1661,7 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 		checkParentLayoutIds.add(LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
 
 		LayoutSet layoutSet = layoutSetPersistence.findByG_P(
-			groupId, privateLayout);
+			groupId, privateLayout, true);
 
 		while (!checkParentLayoutIds.isEmpty()) {
 			long parentLayoutId = checkParentLayoutIds.poll();
@@ -2218,7 +2218,7 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 		throws PortalException {
 
 		LayoutSet layoutSet = layoutSetPersistence.findByG_P(
-			group.getGroupId(), privateLayout);
+			group.getGroupId(), privateLayout, true);
 
 		if (layoutSet.getPageCount() > 0) {
 			return true;
@@ -2240,7 +2240,7 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 					group.getCompanyId(), userGroupClassNameId, userGroupId);
 
 				layoutSet = layoutSetPersistence.findByG_P(
-					userGroupGroup.getGroupId(), privateLayout);
+					userGroupGroup.getGroupId(), privateLayout, true);
 
 				if (layoutSet.getPageCount() > 0) {
 					return true;
