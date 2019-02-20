37d486e5a4a87d40d641a55dcf27647452039f5e
luiz.marins <luiz.f.marins@gmail.com>
2019-02-20 10:20:31 -0300

    LPS-90823 Register DLFileEntry/DLFileVersion to CTConfiguration

--- modules/apps/document-library/document-library-change-tracking/src/main/java/com/liferay/document/library/change/tracking/internal/configuration/DLFileEntryCTConfigurationRegistrar.java
+++ modules/apps/document-library/document-library-change-tracking/src/main/java/com/liferay/document/library/change/tracking/internal/configuration/DLFileEntryCTConfigurationRegistrar.java
@@ -14,8 +14,24 @@
 
 package com.liferay.document.library.change.tracking.internal.configuration;
 
+import com.liferay.change.tracking.configuration.CTConfigurationRegistrar;
+import com.liferay.change.tracking.configuration.builder.CTConfigurationBuilder;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.document.library.kernel.service.DLFileVersionLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceScope;
 
 /**
  * @author Luiz Marins
@@ -25,6 +41,82 @@ public class DLFileEntryCTConfigurationRegistrar {
 
 	@Activate
 	public void activate() {
+		_ctConfigurationRegistrar.register(
+			_builder.setContentType(
+				"Document and Media"
+			).setContentTypeLanguageKey(
+				"document"
+			).setEntityClasses(
+				DLFileEntry.class, DLFileVersion.class
+			).setResourceEntityByResourceEntityIdFunction(
+				_dlFileEntryLocalService::fetchDLFileEntry
+			).setEntityIdsFromResourceEntityFunctions(
+				DLFileEntry::getFileEntryId,
+				this::_versionEntityIdFromResourceEntityFunction
+			).setVersionEntityByVersionEntityIdFunction(
+				_dlFileVersionLocalService::fetchDLFileVersion
+			).setVersionEntityDetails(
+				this::_fetchGroupName, DLFileVersion::getTitle,
+				DLFileVersion::getVersion
+			).setEntityIdsFromVersionEntityFunctions(
+				DLFileVersion::getFileEntryId, DLFileVersion::getFileVersionId
+			).setVersionEntityStatusInfo(
+				new Integer[] {
+					WorkflowConstants.STATUS_APPROVED,
+					WorkflowConstants.STATUS_DRAFT
+				},
+				DLFileVersion::getStatus
+			).build());
 	}
+
+	private String _fetchGroupName(DLFileVersion dlFileVersion) {
+		Group group = _groupLocalService.fetchGroup(dlFileVersion.getGroupId());
+
+		try {
+			return group.getDescriptiveName();
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+
+			return StringPool.BLANK;
+		}
+	}
+
+	private long _versionEntityIdFromResourceEntityFunction(
+		DLFileEntry dlFileEntry) {
+
+		try {
+			DLFileVersion fileVersion = dlFileEntry.getFileVersion();
+
+			return fileVersion.getFileVersionId();
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+
+			return 0L;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DLFileEntryCTConfigurationRegistrar.class);
+
+	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
+	private CTConfigurationBuilder<DLFileEntry, DLFileVersion> _builder;
+
+	@Reference
+	private CTConfigurationRegistrar _ctConfigurationRegistrar;
+
+	@Reference
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Reference
+	private DLFileVersionLocalService _dlFileVersionLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 
 }
