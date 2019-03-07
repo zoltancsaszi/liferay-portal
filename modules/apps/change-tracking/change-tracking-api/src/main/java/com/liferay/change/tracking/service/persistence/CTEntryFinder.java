a65a0887ab30412a08a5035aaec8fec90b3a8268
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-03-07 12:57:49 +0100

    LPS-91698 Autogenerated

--- modules/apps/change-tracking/change-tracking-api/src/main/java/com/liferay/change/tracking/service/persistence/CTEntryFinder.java
+++ modules/apps/change-tracking/change-tracking-api/src/main/java/com/liferay/change/tracking/service/persistence/CTEntryFinder.java
@@ -35,12 +35,12 @@ public interface CTEntryFinder {
 				<com.liferay.change.tracking.model.CTEntry> queryDefinition);
 
 	public java.util.List<com.liferay.change.tracking.model.CTEntry> findByC_R(
-		long ctCollectionId, long resourcePrimKey,
+		long ctCollectionId, long modelResourcePrimKey,
 		com.liferay.portal.kernel.dao.orm.QueryDefinition
 			<com.liferay.change.tracking.model.CTEntry> queryDefinition);
 
 	public com.liferay.change.tracking.model.CTEntry findByC_C_C(
-		long ctCollectionId, long classNameId, long classPK);
+		long ctCollectionId, long modelClassNameId, long modelClassPK);
 
 	public java.util.List<com.liferay.change.tracking.model.CTEntry>
 		findRelatedCTEntries(
