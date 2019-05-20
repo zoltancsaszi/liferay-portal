9c05ea1ee5ded994303918e87aa91c2677f7577b
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-05-20 15:50:05 +0200

    LPS-93194 Apply changes

--- modules/apps/change-tracking/change-tracking-change-lists-web/src/main/java/com/liferay/change/tracking/change/lists/web/internal/display/context/ChangeListsDisplayContext.java
+++ modules/apps/change-tracking/change-tracking-change-lists-web/src/main/java/com/liferay/change/tracking/change/lists/web/internal/display/context/ChangeListsDisplayContext.java
@@ -14,9 +14,9 @@
 
 package com.liferay.change.tracking.change.lists.web.internal.display.context;
 
-import com.liferay.change.tracking.CTEngineManager;
 import com.liferay.change.tracking.configuration.CTConfigurationRegistryUtil;
 import com.liferay.change.tracking.constants.CTPortletKeys;
+import com.liferay.change.tracking.engine.CTEngineManager;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.model.CTEntry;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
